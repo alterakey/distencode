afc3a289c4a988037ed0ab5fd5d776478008a29c
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2012-04-24 10:27:19 +0900

    Taking destination

--- distencode.py
+++ distencode.py
@@ -49,12 +49,12 @@ class Encoder(object):
 ffmpeg -i %(src)s -f yuv4mpegpipe -pix_fmt yuv420p -vf "scale=%(width)d:%(height)d" - | ssh -C %(host)s "cat - > input.mpg && /usr/local/webm/bin/vpxenc input.mpg -o output.webm -p 2  -t 4  --good --cpu-used=0 --target-bitrate=%(bitrate)d --end-usage=vbr --auto-alt-ref=1 --fps=30000/1001 -v --minsection-pct=5 --maxsection-pct=800 --lag-in-frames=16 --kf-min-dist=0 --kf-max-dist=360 --token-parts=2 --static-thresh=0 --drop-frame=0 --min-q=0 --max-q=60 && cat output.webm && rm -f input.mpg output.webm" > interm.%(cookie)s.webm && ffmpeg -i interm.%(cookie)s.webm -i %(src)s -vcodec copy -acodec libvorbis -aq 4 -map 0:v -map 1:a -y %(dest)s && rm -f interm.%(cookie)s.webm
 '''
 
-    def __init__(self, src, bitrate, width, height, host):
+    def __init__(self, src, dest, bitrate, width, height, host):
         self.src = src
         self.bitrate = bitrate
         self.width = width
         self.height = height
-        self.dest = re.sub(u'(mp4|f4v|m4v)$', u'webm', self.src)
+        self.dest = dest
         self.host = host
         self.cookie = hashlib.md5(str(random.getrandbits(256))).hexdigest()
 
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     host = None
     try:
         host = pool.acquire()
-        Encoder(src=sys.argv[1], bitrate=int(sys.argv[2]), width=int(sys.argv[3]), height=int(sys.argv[4]), host=host).encode()
+        Encoder(src=sys.argv[1], dest=sys.argv[2], bitrate=int(sys.argv[3]), width=int(sys.argv[4]), height=int(sys.argv[5]), host=host).encode()
     finally:
         if host is not None:
             pool.release(host)
